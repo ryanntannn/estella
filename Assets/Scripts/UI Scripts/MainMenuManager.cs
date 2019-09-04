f5b3c9730f4f1dde15071fbd3ff6e127e9ffe033
ryan tan <tanryan777@gmail.com>
2019-09-04 14:16:42 +0800

    Started Loading Screen

--- Assets/Scripts/UI Scripts/MainMenuManager.cs
+++ Assets/Scripts/UI Scripts/MainMenuManager.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    enum MainMenuState {
+        TRANSITION,
+        TITLE,
+        SETTINGS
+    }
+
+    MainMenuState mainMenuState = MainMenuState.TITLE;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,24 @@ public class MainMenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(mainMenuState == MainMenuState.TITLE)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                mainMenuState = MainMenuState.TRANSITION;
+                StartCoroutine(StartGame());
+            }
+        }
+        else if (mainMenuState == MainMenuState.SETTINGS)
+        {
+
+        }
+    }
+
+    private IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(1);
     }
 }
+    
