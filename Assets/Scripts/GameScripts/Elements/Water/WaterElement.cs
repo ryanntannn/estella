f75aa36818847e483d0295e52930452665b80b2e
Tan Tiong Guan <tantiongguan123@gmail.com>
2019-09-07 00:18:56 +0800

    Added bolt type and water element

---
+++ Assets/Scripts/GameScripts/Elements/Water/WaterElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterElement : Element {
+    public override void onHit(GameObject other) {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        //finds rigidbody
+        if(rb){ //should not be needed, but just incase
+            rb.velocity += (other.transform.position - transform.position).normalized * 3;
+        }
+        //knockback the target
+    }
+}
---
+++ Assets/Scripts/GameScripts/Types/BoltType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Types/Bolt")]
+public class BoltType : Type {
+    public float maxResource = 5, currentResource, regenRate = 1, depeletionRate = 2;
+    public float fireRate = 1;  //per second
+
+    private float internalCounter;
+    public override void alwaysUpdate(Element element) {
+        currentResource = Mathf.Clamp(currentResource + regenRate * Time.deltaTime, 0, maxResource);
+    }
+
+    public override void start(Element element) {
+        internalCounter = 0;
+        currentResource = maxResource;
+    }
+
+    public override void update(Element element) {
+        if(internalCounter <= 0) {
+            if (Input.GetMouseButton(element.isRightHand ? 1 : 0)) {
+                GameObject instance = Instantiate(element.bolt, element.transform.position, element.transform.rotation);
+                //make a bolt
+                internalCounter = 1 / fireRate; //reset timer
+            }
+        }else {
+            internalCounter -= Time.deltaTime;
+            //lower timer
+        }
+    }
+}
