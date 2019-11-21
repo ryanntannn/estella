70cae4e060e7a176612ca25c7cac78814f4d7525
Ray Chng <10988b@gmail.com>
2019-11-21 09:42:20 +0800

    Commit to Branch 01

--- Assets/Scripts/GameScripts/AI/FSM/Action.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Action : ScriptableObject {
-    public abstract void DoAction(FSMController agent);
-}
--- Assets/Scripts/GameScripts/AI/FSM/State.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu (menuName = "AI/State")]
-public class State : ScriptableObject {
-    public Transition[] transitions;
-}
