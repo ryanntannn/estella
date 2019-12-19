f2ad2a2fcb5af5f0c25ae369b25d71a2e72db289
Ray Chng <10988b@gmail.com>
2019-12-19 17:07:27 +0800

    Commit Terrain 1
    Complete of terrian 1 with addition of particle effects

---
+++ Assets/Resources/Elements/Wind/Windslash/Rotate_obk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_obk : MonoBehaviour
+{
+    public GameObject slashObj;
+    public Vector3 Rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        slashObj.transform.Rotate(Rotation, Space.World);
+    }
+}
