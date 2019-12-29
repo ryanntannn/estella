e56b50f474b49a9b418d85d6352fb71d693d9e68
Tan Tiong Guan <tantiongguan123@gmail.com>
2019-12-30 02:01:03 +0800

    Did work on algorithms

--- Assets/Scripts/GameScripts/AI/PathFinding/Algorithms.cs
+++ Assets/Scripts/GameScripts/AI/PathFinding/Algorithms.cs
@@ -4,19 +4,84 @@ using UnityEngine;
 using System;
 
 public static class Algorithms {
-    //public static List<Node> Dijkstras(MapGrid grid, Vector3 start, Vector3 end) {
-    //    Node startNode = grid.WorldPointToNode(start);
-    //    Node endNode = grid.WorldPointToNode(end);
+    public static List<Node> Dijkstras(MapGrid grid, Vector3 start, Vector3 end, int threshold = 1000) {
+        Node startNode = grid.WorldPointToNode(start);
+        Node endNode = grid.WorldPointToNode(end);
+        //make sure walkable
+        startNode = FindClosestWalkable(grid, startNode);
+        endNode = FindClosestWalkable(grid, endNode);
 
-    //    Queue<Node> openList = new Queue<Node>();
-    //    List<Node> closedList = new List<Node>();
-    //    openList.Enqueue(startNode);
-    //    Node currentNode = openList.Dequeue();
-    //}
+        Queue<Node> openList = new Queue<Node>();
+        List<Node> closedList = new List<Node>();
+        openList.Enqueue(startNode);
+        int count = 0;
+        while (openList.Count > 0)  {
+            if(++count >= threshold) {
+                throw new AlgorithmExecption("Threshold reached");
+            }
+
+            if (closedList.Contains(endNode)) break;
+
+            Node currentNode = openList.Dequeue();
+            //neighbours
+            foreach(Node n in grid.GetNeighbours(currentNode)) {
+                if (n.walkable && !closedList.Contains(n)) {
+                    n.parentNode = currentNode;
+                    openList.Enqueue(n);
+                }
+            }
+
+            //add to closed list
+            closedList.Add(currentNode);
+        }
+
+        return TracePath(startNode, endNode);
+    }
+
+    public static List<Node> Dijkstras(MapGrid grid, Vector3 start, Vector3[] end, int threshold = 1000) {
+        Node startNode = grid.WorldPointToNode(start);
+        startNode = FindClosestWalkable(grid, startNode);
+        Node[] endNodes = new Node[end.Length];
+        for(int count = 0; count <= end.Length - 1; count++) {
+            endNodes[count] = FindClosestWalkable(grid, grid.WorldPointToNode(end[count]));
+        }
+
+        Queue<Node> openList = new Queue<Node>();
+        List<Node> closedList = new List<Node>();
+        openList.Enqueue(startNode);
+        int i = 0;
+        while (openList.Count > 0) {
+            if (++i >= threshold) {
+                throw new AlgorithmExecption("Threshold reached");
+            }
+
+            //check if any of the items in closed list match the end
+            foreach(Node n in endNodes) {
+                if (closedList.Contains(n)) return TracePath(startNode, n);
+            }
+
+            Node currentNode = openList.Dequeue();
+            //neighbours
+            foreach (Node n in grid.GetNeighbours(currentNode)) {
+                if (n.walkable && !closedList.Contains(n)) {
+                    n.parentNode = currentNode;
+                    openList.Enqueue(n);
+                }
+            }
+
+            //add to closed list
+            closedList.Add(currentNode);
+        }
+
+        //item not found
+        return null;
+    }
 
     public static List<Node> AStar(MapGrid grid, Vector3 start, Vector3 end, int threshold = 1000) {
         Node startNode = grid.WorldPointToNode(start);
         Node endNode = grid.WorldPointToNode(end);
+        startNode = FindClosestWalkable(grid, startNode);
+        endNode = FindClosestWalkable(grid, endNode);
 
         PiorityQueue<Node> openList = new PiorityQueue<Node>();
         List<Node> closedList = new List<Node>();
@@ -26,7 +91,7 @@ public static class Algorithms {
         while (openList.Count > 0) {
             //error checking
             if (++count > threshold) {
-                throw new AStarException("Threshold reached");
+                throw new AlgorithmExecption("Threshold reached");
             }
 
             if (closedList.Contains(endNode)) break;
@@ -63,16 +128,7 @@ public static class Algorithms {
             closedList.Add(currentNode);
         }
 
-        //make path
-        List<Node> path = new List<Node>();
-        Node pathNode = endNode;
-        while (pathNode != startNode) {
-            path.Add(pathNode);
-            pathNode = pathNode.parentNode;
-        }
-        path.Add(startNode);
-        path.Reverse();
-        return path;
+        return TracePath(startNode, endNode);
     }
 
     private static float GetManhatten(MapGrid grid, Node start, Node end) {
@@ -90,8 +146,31 @@ public static class Algorithms {
         float eucDist = (start.gridPos - end.gridPos).magnitude;
         return eucDist / gridSize.magnitude;
     }
+
+    private static Node FindClosestWalkable(MapGrid grid, Node _input) {
+        if (_input.walkable) return _input; //this is walkable
+
+        //find neighbours
+        foreach(Node n in grid.GetNeighbours(_input)) {
+            return FindClosestWalkable(grid, n);
+        }
+
+        return null;
+    }
+
+    private static List<Node> TracePath(Node _start, Node _end) {
+        List<Node> path = new List<Node>();
+        Node currentNode = _end;
+        while(currentNode != _start) {
+            path.Add(currentNode);
+            currentNode = currentNode.parentNode;
+        }
+        path.Add(_start);
+        path.Reverse();
+        return path;
+    }
 }
 
-class AStarException : Exception {
-    public AStarException(string _message) : base(_message) { }
+class AlgorithmExecption : Exception {
+    public AlgorithmExecption(string _message) : base(_message) { }
 }
