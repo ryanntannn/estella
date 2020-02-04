10950fbaefd0cbb19be57714c3fbb688bf5a7af4
ryanntannn <tanryan777@gmail.com>
2020-02-04 16:14:24 +0800

    Quest and Subquest ScriptableObjects

---
+++ Assets/Scripts/GameScripts/Quest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Quest : ScriptableObject
+{
+    public string shortDesc;
+    public string longDesc;
+    public List<SubQuest> subQuests;
+}
---
+++ Assets/Scripts/GameScripts/SubQuest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SubQuestType
+{
+    Locate, Kill, Collect
+}
+
+[CreateAssetMenu]
+public class SubQuest : ScriptableObject
+{
+    public string shortDesc;
+    public string longDesc;
+    public SubQuestType questType;
+    public List<GameObject> objective;
+    public int amount;
+}
