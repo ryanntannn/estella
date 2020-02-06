dcc6da5710610268166453620c96ff382827ec72
Depressionn <tantiongguan123@gmail.com>
2020-02-06 11:06:52 +0800

    plasma correct now

--- Assets/Scripts/GameScripts/ElementStuff/Water.cs
+++ Assets/Scripts/GameScripts/ElementStuff/Water.cs
@@ -10,6 +10,10 @@ public class Water : Element {
 
     public override string ElementName { get { return "Water"; } }
 
+	public override int SmallAttackCost => 6;
+
+	public override int BigAttackCost => 10;
+
 	public override void DoBasic(ElementControl agent, Hand hand) {
 		//bubble shot
 		GameObject instance = Instantiate(Resources.Load<GameObject>("Elements/Water/BubbleShot"), hand.handPos.position, hand.transform.rotation);
@@ -19,12 +23,14 @@ public class Water : Element {
 			Vector3 toLookAt = hitInfo.point;
 			toLookAt.y = instance.transform.position.y;
 			instance.transform.LookAt(toLookAt);
-		} 
+		}
+		agent.currentMana -= SmallAttackCost;
 	}
 
 	public override void DoBig(ElementControl agent, Hand hand) {
 		//tsunami
 		GameObject tsunami = Resources.Load<GameObject>("Elements/Water/Tsunami");
 		tsunami = Instantiate(tsunami, agent.transform.position - agent.transform.forward - agent.transform.up, agent.transform.rotation);
+		agent.currentMana -= BigAttackCost;
 	}
 }
