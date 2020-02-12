d53306d810a49b4b63d741b3c7334e81c46d1061
ryanntannn <tanryan777@gmail.com>
2020-02-12 18:51:49 +0800

    Make Minimap relative to camera pivot

---
+++ Assets/Scripts/GameScripts/MinimapCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform cameraPivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPivot = GameObject.Find("camera-pivot").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.eulerAngles = new Vector3(90, cameraPivot.eulerAngles.y, 0);
+    }
+}
