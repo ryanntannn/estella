20d76c27c0e92f13923235691abfe7863a01d6ef
ryanntannn <tanryan777@gmail.com>
2020-02-13 16:15:41 +0800

    Giant Boss can Attack and Take Damage

---
+++ Assets/Scripts/GameScripts/AI/Giant Boss/GiantBoss.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantBoss : MonoBehaviour
+{
+    public Enemy bossBody;
+    public BossArm[] bossArms;
+    public GameObject player;
+
+    public bool isAttacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossBody = transform.GetChild(0).GetComponent<Enemy>();
+        bossArms = transform.GetComponentsInChildren<BossArm>();
+        foreach(BossArm ba in bossArms)
+        {
+            ba.giantBoss = this;
+        }
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAttacking)
+        {
+            int randArm = (int)Random.Range(0, 4);
+            int randMove = 0;//(int)Random.Range(0, 2);
+            //Attack
+            if (randMove == 0)
+            {
+                Debug.Log(randArm + " " + randMove);
+                bossArms[randArm].Slam(player.transform.position);
+            }
+            else
+            {
+                bossArms[randArm].Swing(player.transform.position);
+            }
+
+            isAttacking = true;
+        }
+    }
+}
