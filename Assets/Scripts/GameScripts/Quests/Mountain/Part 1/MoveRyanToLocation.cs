8d4b0a9ad046239f177835089f2f8c75dd7b8e47
Ray Chng <10988b@gmail.com>
2020-02-15 21:41:41 +0800

    Quest System

---
+++ Assets/Scripts/GameScripts/Quests/Mountain/Part 1/MoveRyanToLocation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRyanToLocation : MonoBehaviour {
+    public GameObject toMove;
+    public GameObject moveTo;
+
+    private InteractableObject m_interectionHook;
+    // Start is called before the first frame update
+    void Start() {
+        m_interectionHook = GetComponent<InteractableObject>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (m_interectionHook.isActivated) {
+            toMove.SetActive(false);
+            moveTo.SetActive(true);
+            Destroy(this);
+        }
+    }
+}
