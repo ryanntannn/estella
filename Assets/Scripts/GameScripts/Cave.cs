66d6bd5da66160bdfd52b084dbb0d6f503955e4b
ryanntannn <tanryan777@gmail.com>
2020-02-16 22:24:56 +0800

    Finished Up Scene 2

---
+++ Assets/Scripts/GameScripts/Cave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cave : MonoBehaviour
+{
+    InteractableObject interactableObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactableObject = GetComponent<InteractableObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LevelManager.Instance.completedQuests.Contains(8) && interactableObject.isActivated)
+        {
+            //Load Scene 3
+        } else
+        {
+            interactableObject.isActivated = false;
+        }
+    }
+}
