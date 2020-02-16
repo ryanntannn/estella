048669fa98d425d0256080a47506cfaa56d2e10a
Ray Chng <10988b@gmail.com>
2020-02-16 17:23:35 +0800

    Add Prefabs in scene 3 and Questing as well

---
+++ Assets/Scripts/UI Scripts/Ray's Ui Scripts/Timeline Script/Timelinetrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Timelinetrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       // timeline = GetComponent<PlayableDirector>();
+        print("Name of timeline is " + timeline.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ void OnTriggerEnter(Collider player)
+     {
+        if (player.gameObject.tag == "Player")
+        {
+            print("Trying to play");
+            timeline.Play();
+
+        }
+        
+     }
+}
