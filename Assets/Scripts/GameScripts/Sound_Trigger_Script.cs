b71e127e38302f5d339f06f6852bc604d2cae3f1
Ray Chng <10988b@gmail.com>
2020-02-16 22:04:06 +0800

    Sound effects

---
+++ Assets/Scripts/GameScripts/Sound_Trigger_Script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Trigger_Script : MonoBehaviour
+{
+    public bool PlaysoundonEnter;
+    public bool CheckforExit;
+    public AudioSource soundsource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  AudioSource soundsource = GetComponent<AudioSource>();
+        soundsource.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   void OnTriggerEnter( Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (PlaysoundonEnter == true && soundsource.isPlaying == false)
+            {
+                soundsource.Play();
+            }
+
+            if (PlaysoundonEnter == true && soundsource.isPlaying == false)
+            {
+                soundsource.Stop();
+            }
+
+        }
+    }
+
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && CheckforExit == true)
+        {
+         
+
+            
+                soundsource.Stop();
+            
+
+        }
+    }
+}
+
+
+
