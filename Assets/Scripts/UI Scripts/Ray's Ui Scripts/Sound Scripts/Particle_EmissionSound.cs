f63a4e1292d9b4366219185b3af2ffb4674558b9
Ray Chng <10988b@gmail.com>
2020-02-18 00:29:33 +0800

    Initial Commit(With Lightning prefabs)

---
+++ Assets/Scripts/UI Scripts/Ray's Ui Scripts/Sound Scripts/Particle_EmissionSound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_EmissionSound : MonoBehaviour
+{
+    public ParticleSystem parentParticleSystem;
+
+    private int currentNumberOfParticles = 0;
+
+    public AudioSource BornSounds;
+    public AudioSource DieSounds;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // parentParticleSystem = this.GetComponent<ParticleSystem>();
+        if (parentParticleSystem == null)
+            Debug.LogError("Missing ParticleSystem!", this);
+
+        print("Name of Parent Particle " + parentParticleSystem.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var amount = Mathf.Abs(currentNumberOfParticles - parentParticleSystem.particleCount);
+
+        if (parentParticleSystem.particleCount < currentNumberOfParticles)
+        {
+            
+           //Play Destroyed Sound
+        }
+
+        print("Particle Count "  + parentParticleSystem.particleCount);
+
+        if (parentParticleSystem.particleCount > currentNumberOfParticles)
+        {
+            print("PlayBornSounds");
+            BornSounds.Play();
+            //Play born sound
+        }
+
+        currentNumberOfParticles = parentParticleSystem.particleCount;
+    }
+}
