ec6a560e6b242ecf4a0e19f7e228640d031f12cc
Ray Chng <10988b@gmail.com>
2020-02-17 12:46:11 +0800

    Add Faded Sound

---
+++ Assets/Scripts/UI Scripts/Ray's Ui Scripts/Checkpoint/Checkpoint.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class set the checkpoint of the object, when player dies it goes back to the last know location
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject player;
+
+    private PlayerControl playerControl;
+    private Animator animator;
+    private Vector3 checkpointCoordinates;
+    // Start is called before the first frame update
+    void Start()
+    {
+     playerControl  =  player.GetComponent<PlayerControl>();
+     animator = player.GetComponentInChildren<Animator>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown("k"))
+        {
+            print("Pressed K");
+            ResetToLastKnownLocation();
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Checkpoint")
+        {
+            checkpointCoordinates = collider.transform.position;
+            print("Checkpoint Collided");
+        }
+
+    }
+
+  public  void ResetToLastKnownLocation() //reset to last known location
+    {
+        RevivePlayer();
+        player.transform.position = checkpointCoordinates;
+        playerControl.enabled = true;
+        animator.SetTrigger("Respawn");
+    }
+
+
+    void RevivePlayer() //reser theri health
+    {
+        playerControl.currentStamina = 100f;
+        playerControl.currentHealth = 100f;
+    }
+
+}
---
+++ Assets/Scripts/UI Scripts/Ray's Ui Scripts/Sound Scripts/AudioFade.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class AudioFade
+{
+    
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0 || audioSource.isPlaying == false)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+
+    public static IEnumerator FadeIn(AudioSource audioSource, float FadeTime, float volume)
+    {
+        
+        float startVolume = 0.2f;
+
+       
+
+        audioSource.volume = 0;
+       // audioSource.Stop();
+        audioSource.Play();
+
+        while (audioSource.volume < volume)
+        {
+            audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.volume = volume;
+    }
+}
+
+
