9cf71d7886446ac353214fb87020d6242869b46d
Ray Chng <10988b@gmail.com>
2020-02-18 23:55:18 +0800

    Fixed Everthing possible but still shit :anguished:

---
+++ Assets/CheckForKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForKey : MonoBehaviour
+{
+
+     public GameObject MagicBarrier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && LevelManager.Instance.completedQuests.Contains(3) )
+        {
+            MagicBarrier.SetActive(false);
+            IngameUI.Instance.ShowPopUp("Magic Barrier Dispeled", 3.0f);
+        }
+    }
+}
---
+++ Assets/NameTagScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameTagScript : MonoBehaviour
+{
+    IngameUI igui;
+
+    private bool single = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        igui = GameObject.Find("UI (1)").GetComponent<IngameUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (single == false)
+        {
+            ShowName();
+        }
+        single = true;
+    }
+
+    void ShowName()
+    {
+        igui.ShowBigPopUp("-" + "Skylark the Usurper" + "-", 4f);
+    }
+}
