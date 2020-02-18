d2255b2ad78d8b02adc230eb1dfd3908aaa98e39
Depressionn <tantiongguan123@gmail.com>
2020-02-18 19:06:52 +0800

    .

---
+++ Assets/Scripts/UI Scripts/Ray's Ui Scripts/Boss Additional/CueThySkylark.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueThySkylark : MonoBehaviour
+{
+    public GameObject Skylark;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (Skylark.activeSelf == false)
+            {
+                Skylark.SetActive(true);
+
+            }
+        }
+    }
+}
